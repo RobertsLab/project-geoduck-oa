d0a7611ab4dc3b3e051ffad975d6a49b888786c8
Hollie Putnam <hollieputnam@gmail.com>
2016-03-04 22:43:00 -0800

    New seawater chemistry test data and script updates

---
+++ RAnalysis/Scripts/Full_SW_Chem.R
@@ -0,0 +1,179 @@
+#project-geoduck-oa
+#Data from Roberts et al NOAA OA
+#last modified 20160304 H Putnam
+
+
+rm(list=ls()) # removes all prior objects
+
+#Read in required libraries
+library("seacarb") #seawater carbonate chemistry
+library("reshape") #reshape data
+library("plotrix") #functions in tapply
+
+#############################################################
+setwd("/Users/hputnam/MyProjects/Geoduck_Epi/project-geoduck-oa/RAnalysis/Data/") #set working directory
+
+#Required Data files
+#pH_Calibration_Files/
+#SW_Chem.csv
+
+#SEAWATER CHEMISTRY ANALYSIS FOR DISCRETE MEASUREMENTS
+
+#For conversion equations for pH from mV to total scale using tris standard
+
+path <-("/Users/hputnam/MyProjects/Geoduck_Epi/project-geoduck-oa/RAnalysis/Data/pH_Calibration_Files/")
+
+#list all the file names in the folder to get only get the csv files
+file.names<-list.files(path = path, pattern = "csv$")
+
+pH.cals <- data.frame(matrix(NA, nrow=length(file.names), ncol=4, dimnames=list(file.names,c("Date", "Intercept", "Slope","R2")))) #generate a 3 column dataframe with specific column names
+
+for(i in 1:length(file.names)) { # for every file in list start at the first and run this following function
+  Calib.Data <-read.table(file.path(path,file.names[i]), header=TRUE, sep=",", na.string="NA", as.is=TRUE) #reads in the data files
+  model <-lm(mVTris ~ TTris, data=Calib.Data) #runs a linear regression of mV as a function of temperature
+  coe <- coef(model) #extracts the coeffecients
+  R <- summary(model)$r.squared #extracts the R2
+  pH.cals[i,2:3] <- coe #inserts coef in the dataframe
+  pH.cals[i,4] <- R #inserts R2 in the dataframe
+  pH.cals[i,1] <- substr(file.names[i],1,8) #stores the file name in the Date column
+}
+
+colnames(pH.cals) <- c("Calib.Date",  "Intercept",  "Slope", "R2")
+pH.cals
+
+# read in total alkalinity, temperature, and salinity
+SW.chem <- read.csv("SW_Chem.csv", header=TRUE, sep=",", na.strings="NA") #load data with a header, separated by commas, with NA as NA
+
+#merge with Seawater chemistry file
+SW.chem <- merge(pH.cals, SW.chem, by="Calib.Date")
+
+#constants for use in pH calculation 
+R <- 8.31447215 #gas constant in J mol-1 K-1 
+F <-96485.339924 #Faraday constant in coulombs mol-1
+
+mvTris <- SW.chem$Temperature*SW.chem$Slope+SW.chem$Intercept #calculate the mV of the tris standard using the temperature mv relationships in the measured standard curves 
+STris<-27.0 #salinity of the Tris
+phTris<- (11911.08-18.2499*STris-0.039336*STris^2)*(1/(SW.chem$Temperature+273.15))-366.27059+ 0.53993607*STris+0.00016329*STris^2+(64.52243-0.084041*STris)*log(SW.chem$Temperature+273.15)-0.11149858*(SW.chem$Temperature+273.15) #calculate the pH of the tris (Dickson A. G., Sabine C. L. and Christian J. R., SOP 6a)
+SW.chem$pH.Total<-phTris+(mvTris/1000-SW.chem$pH.MV/1000)/(R*(SW.chem$Temperature+273.15)*log(10)/F) #calculate the pH on the total scale (Dickson A. G., Sabine C. L. and Christian J. R., SOP 6a)
+
+#Calculate CO2 parameters using seacarb
+carb.output <- carb(flag=8, var1=SW.chem$pH.Total, var2=SW.chem$TA/1000000, S= SW.chem$Salinity, T=SW.chem$Temperature, P=0, Pt=0, Sit=0, pHscale="T", kf="pf", k1k2="l", ks="d") #calculate seawater chemistry parameters using seacarb
+
+carb.output$ALK <- carb.output$ALK*1000000 #convert to µmol kg-1
+carb.output$CO2 <- carb.output$CO2*1000000 #convert to µmol kg-1
+carb.output$HCO3 <- carb.output$HCO3*1000000 #convert to µmol kg-1
+carb.output$CO3 <- carb.output$CO3*1000000 #convert to µmol kg-1
+carb.output$DIC <- carb.output$DIC*1000000 #convert to µmol kg-1
+
+carb.output <- cbind(SW.chem$Measure.Date,  SW.chem$Tank,  SW.chem$Treatment, carb.output) #combine the sample information with the seacarb output
+colnames(carb.output) <- c("Date",  "Tank",  "Treatment",	"flag",	"Salinity",	"Temperature",	"Pressure",	"pH",	"CO2",	"pCO2",	"fCO2",	"HCO3",	"CO3",	"DIC", "TA",	"Aragonite.Sat", 	"Calcite.Sat") #Rename columns to describe contents
+carb.output <- subset(carb.output, select= c("Date",  "Tank",  "Treatment",	"Salinity",	"Temperature",		"pH",	"CO2",	"pCO2",	"HCO3",	"CO3",	"DIC", "TA",	"Aragonite.Sat"))
+
+#Tanks
+mean_pCO2=tapply(carb.output$pCO2, carb.output$Tank, mean)
+se_pCO2=tapply(carb.output$pCO2, carb.output$Tank, std.error)
+mean_Temp=tapply(carb.output$Temperature, carb.output$Tank, mean)
+se_Temp=tapply(carb.output$Temperature, carb.output$Tank, std.error)
+mean_Sal=tapply(carb.output$Salinity, carb.output$Tank, mean)
+se_Sal=tapply(carb.output$Salinity, carb.output$Tank, std.error)
+mean_TA=tapply(carb.output$TA, carb.output$Tank, mean)
+se_TA=tapply(carb.output$TA, carb.output$Tank, std.error)
+mean_pH=tapply(carb.output$pH, carb.output$Tank, mean)
+se_pH=tapply(carb.output$pH, carb.output$Tank, std.error)
+mean_DIC=tapply(carb.output$DIC, carb.output$Tank, mean)
+se_DIC=tapply(carb.output$DIC, carb.output$Tank, std.error)
+
+#Treatments
+gmean_pCO2 <- tapply(carb.output$pCO2, carb.output$Treatment, mean)
+gse_pCO2 <- tapply(carb.output$pCO2, carb.output$Treatment, std.error)
+gmean_Temp <- tapply(carb.output$Temperature, carb.output$Treatment, mean)
+gse_Temp <- tapply(carb.output$Temperature, carb.output$Treatment, std.error)
+gmean_Sal <- tapply(carb.output$Salinity, carb.output$Treatment, mean)
+gse_Sal <- tapply(carb.output$Salinity, carb.output$Treatment, std.error)
+gmean_TA <- tapply(carb.output$TA, carb.output$Treatment, mean)
+gse_TA <- tapply(carb.output$TA, carb.output$Treatment, std.error)
+gmean_pH <- tapply(carb.output$pH, carb.output$Treatment, mean)
+gse_pH <- tapply(carb.output$pH, carb.output$Treatment, std.error)
+gmean_DIC <- tapply(carb.output$DIC, carb.output$Treatment, mean)
+gse_DIC <- tapply(carb.output$DIC, carb.output$Treatment, std.error)
+
+mean.carb.output <- rbind(gmean_pCO2, gse_pCO2, gmean_pH, gse_pH, gmean_Temp, gse_Temp, gmean_Sal, gse_Sal, gmean_TA, gse_TA, gmean_DIC, gse_DIC)
+mean.carb.output <- as.data.frame(mean.carb.output)
+row.names(mean.carb.output) <- c("mean pCO2", "SE pCO2", "mean pH", "SE pH", "mean Temperature", "SE Temperature", "mean Salinity", "SE Salinity", "mean Total Alkalinity", "SE Total Alkalinity", "mean DIC", "SE DIC")
+mean.carb.output$Variables <- row.names(mean.carb.output)
+write.table (mean.carb.output, file="/Users/hputnam/MyProjects/Geoduck_Epi/project-geoduck-oa/RAnalysis/Output/Seawater_chemistry_table_Output.csv", sep=",", row.names = FALSE)
+
+#Plot Tank and Treatment mean ± se
+
+pdf("/Users/hputnam/MyProjects/Geoduck_Epi/project-geoduck-oa/RAnalysis/Output/running_carbonate_chemistry.pdf")
+par(cex.axis=0.8, cex.lab=0.8, mar=c(5, 5, 4, 2),mgp=c(3.7, 0.8, 0),las=1, mfrow=c(4,3))
+
+#Tanks
+plot(c(1,6),c(0,2200),type="n",ylab=expression(paste("pCO"["2"])), xlab=expression(paste("Tank")))
+plotCI(x=c(1,2,3,4,5,6), y=mean_pCO2,uiw=se_pCO2, liw=se_pCO2,add=TRUE,gap=0.001)
+abline(h=540, lty=2)
+abline(h=1500, lty=2)
+
+plot(c(1,6),c(6,9),type="n",ylab=expression(paste("pH")), xlab=expression(paste("Tank")))
+plotCI(x=c(1,2,3,4,5,6), y=mean_pH,uiw=se_pH, liw=se_pH,add=TRUE,gap=0.001)
+abline(h=7.50, lty=2)
+abline(h=7.90, lty=2)
+
+plot(c(1,6),c(12,15),type="n",ylab=expression(paste("Temperature °C")), xlab=expression(paste("Tank")))
+plotCI(x=c(1,2,3,4,5,6), y=mean_Temp,uiw=se_Temp, liw=se_Temp,add=TRUE,gap=0.001)
+abline(h=14, lty=2)
+abline(h=14, lty=2)
+
+plot(c(1,6),c(25,29),type="n",ylab=expression(paste("Salinity")), xlab=expression(paste("Tank")))
+plotCI(x=c(1,2,3,4,5,6), y=mean_Sal,uiw=se_Sal, liw=se_Sal,add=TRUE,gap=0.001)
+abline(h=27, lty=2)
+abline(h=27, lty=2)
+
+plot(c(1,6),c(1800,2200),type="n",ylab=expression(paste("TA µmol kg-1")), xlab=expression(paste("Tank")))
+plotCI(x=c(1,2,3,4,5,6), y=mean_TA,uiw=se_TA, liw=se_TA,add=TRUE,gap=0.001)
+abline(h=2085, lty=2)
+abline(h=2085, lty=2)
+
+plot(c(1,6),c(1800,2200),type="n",ylab=expression(paste("DIC µmol kg-1")), xlab=expression(paste("Tank")))
+plotCI(x=c(1,2,3,4,5,6), y=mean_DIC,uiw=se_DIC, liw=se_DIC,add=TRUE,gap=0.001)
+abline(h=1990, lty=2)
+abline(h=2100, lty=2)
+
+#Treatments
+plot(c(1,2),c(0,2200), xaxt = "n", type="n",ylab=expression(paste("pCO"["2"])), xlab=expression(paste("Treatment")))
+axis(1, at=1:2, labels=c("Ambient", "High"))
+plotCI(x=c(1,2), y=gmean_pCO2,uiw=gse_pCO2, liw=gse_pCO2,add=TRUE,gap=0.001)
+abline(h=540, lty=2)
+abline(h=1500, lty=2)
+
+plot(c(1,2),c(6,9), xaxt = "n", type="n",ylab=expression(paste("pH")), xlab=expression(paste("Treatment")))
+axis(1, at=1:2, labels=c("Ambient", "High"))
+plotCI(x=c(1,2), y=gmean_pH,uiw=gse_pH, liw=gse_pH,add=TRUE,gap=0.001)
+abline(h=7.50, lty=2)
+abline(h=7.90, lty=2)
+
+plot(c(1,2),c(12,15), xaxt = "n", type="n",ylab=expression(paste("Temperature °C")), xlab=expression(paste("Treatment")))
+axis(1, at=1:2, labels=c("Ambient", "High"))
+plotCI(x=c(1,2), y=gmean_Temp,uiw=gse_Temp, liw=gse_Temp,add=TRUE,gap=0.001)
+abline(h=14, lty=2)
+abline(h=14, lty=2)
+
+plot(c(1,2),c(25,29), xaxt = "n", type="n",ylab=expression(paste("Salinity")), xlab=expression(paste("Treatment")))
+axis(1, at=1:2, labels=c("Ambient", "High"))
+plotCI(x=c(1,2), y=gmean_Sal,uiw=gse_Sal, liw=gse_Sal,add=TRUE,gap=0.001)
+abline(h=27, lty=2)
+abline(h=27, lty=2)
+
+plot(c(1,2),c(1800,2200), xaxt = "n", type="n",ylab=expression(paste("TA µmol kg-1")), xlab=expression(paste("Treatment")))
+axis(1, at=1:2, labels=c("Ambient", "High"))
+plotCI(x=c(1,2), y=gmean_TA,uiw=gse_TA, liw=gse_TA,add=TRUE,gap=0.001)
+abline(h=2085, lty=2)
+abline(h=2085, lty=2)
+
+plot(c(1,2),c(1800,2200), xaxt = "n", type="n",ylab=expression(paste("DIC µmol kg-1")), xlab=expression(paste("Treatment")))
+axis(1, at=1:2, labels=c("Ambient", "High"))
+plotCI(x=c(1,2), y=gmean_DIC,uiw=gse_DIC, liw=gse_DIC,add=TRUE,gap=0.001)
+abline(h=1985, lty=2)
+abline(h=2085, lty=2)
+
+dev.off()
